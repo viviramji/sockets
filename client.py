88f62e734fabfdf39d73821b2c40844d06f48304
viviramji <viviramji@gmail.com>
2019-09-16 12:40:52 -0500

    Header and how to give some info to client.py

--- client.py
+++ client.py
@@ -1,13 +1,26 @@
 import socket
 
+HEADERSIZE = 10
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
-full_msg = ''
 
 while True:
-    msg = s.recv(8)
-    if len(msg) <= 0:
-        break
-    full_msg += msg.decode('utf-8')
-    
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print(f"New message length is: {msg[:HEADERSIZE]}")
+            msglen = int(msg[:HEADERSIZE].decode('utf-8'))
+            new_msg = False
+
+        full_msg += msg.decode("utf-8")
+
+        if len(full_msg) - HEADERSIZE == msglen:
+            print("Full message recv")
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ''
--- server.py
+++ server.py
@@ -1,4 +1,9 @@
 import socket
+import time
+#Header is for give some information, e.g. how long is ur msg
+
+HEADERSIZE = 10
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
@@ -7,5 +12,14 @@ s.listen(5)
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Hello from the server", 'utf-8'))
-    clientsocket.close()
+
+    msg = "Hello from the server"
+    msg = f"{len(msg):<{HEADERSIZE}}" + msg
+    
+    clientsocket.send(bytes(msg, 'utf-8'))
+    
+    while True:
+        time.sleep(3)
+        msg = f"The time is: {time.time()}"
+        msg = f"{len(msg):<{HEADERSIZE}}" + msg
+        clientsocket.send(bytes(msg, 'utf-8'))
